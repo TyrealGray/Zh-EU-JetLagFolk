6176180b4e337f49ca603a15996bf0a8fe076d8f
tyrealgray <tyrealgray@ymail.com>
2016-12-20 22:10:27 +0100

    update new member

--- assets/js/data/folks.js
+++ assets/js/data/folks.js
@@ -1,73 +1,79 @@
 export default {
-    folks:[{
-        name:'司马缸',
-        qq:'312846541',
-        steam:'http://steamcommunity.com/id/tyrealgray',
-        platform:'xbox(TyrealGray), steam',
-        wanted:'Battlerite, HELLDIVERS, Rainbow Six Siege(xbox), Dying Light(xbox), Overwatch(xbox)'
-    },{
-        name:'郭一年',
-        qq:'971395764',
-        steam:'http://steamcommunity.com/id/guoyinian',
-        platform:'steam',
-        wanted:'Battlerite, HELLDIVERS, Terraria'
-    },{
-        name:'柴鱼精',
-        qq:'823404031',
-        steam:'https://steamcommunity.com/profiles/76561198336102572',
-        platform:'steam',
-        wanted:'Dead by Daylight, Don\'t Starve Together'
-    },{
-        name:'HUIB',
-        qq:'544040151',
-        steam:'http://steamcommunity.com/profiles/76561198250406440',
-        platform:'steam',
-        wanted:'Battlerite'
+    folks: [{
+        name: '司马缸',
+        qq: '312846541',
+        steam: 'http://steamcommunity.com/id/tyrealgray',
+        platform: 'xbox(TyrealGray), steam',
+        wanted: 'Battlerite, HELLDIVERS, Rainbow Six Siege(xbox), Dying Light(xbox), Overwatch(xbox)'
+    }, {
+        name: '郭一年',
+        qq: '971395764',
+        steam: 'http://steamcommunity.com/id/guoyinian',
+        platform: 'steam',
+        wanted: 'Battlerite, HELLDIVERS, Terraria'
+    }, {
+        name: '柴鱼精',
+        qq: '823404031',
+        steam: 'https://steamcommunity.com/profiles/76561198336102572',
+        platform: 'steam',
+        wanted: 'Dead by Daylight, Don\'t Starve Together'
+    }, {
+        name: 'HUIB',
+        qq: '544040151',
+        steam: 'http://steamcommunity.com/profiles/76561198250406440',
+        platform: 'steam',
+        wanted: 'Battlerite'
     },
     {
-        name:'WGL',
-        qq:'380866845',
-        platform:'ps(xxzlu2) ,steam',
-        steam:'http://steamcommunity.com/profiles/76561198079607263',
-        wanted:'Battlerite'
+        name: 'WGL',
+        qq: '380866845',
+        platform: 'ps(xxzlu2) ,steam',
+        steam: 'http://steamcommunity.com/profiles/76561198079607263',
+        wanted: 'Battlerite'
     },
     {
-        name:'IU',
-        qq:'564759051',
-        platform:'steam',
-        steam:'http://steamcommunity.com/profiles/76561198330683065',
-        wanted:'Dead by Daylight, Don\'t Starve Together, Battlerite'
+        name: '小迷妹',
+        qq: '564759051',
+        platform: 'steam',
+        steam: 'http://steamcommunity.com/profiles/76561198330683065',
+        wanted: 'Dead by Daylight, Don\'t Starve Together, Battlerite'
     },
     {
-        name:'Gin',
-        qq:'191619884',
-        steam:'http://steamcommunity.com/profiles/76561198089957998',
-        platform:'steam',
-        wanted:'astroneer, GTA5, HELLDIVERS, Terraria'
-    },{
-        name:'横空飞禹',
-        qq:'526733527',
-        steam:'http://steamcommunity.com/id/liuame',
-        platform:'steam',
-        wanted:'Don\'t Starve Together, GTA5,'
+        name: 'Gin',
+        qq: '191619884',
+        steam: 'http://steamcommunity.com/profiles/76561198089957998',
+        platform: 'steam',
+        wanted: 'astroneer, GTA5, HELLDIVERS, Terraria'
+    }, {
+        name: '横空飞禹',
+        qq: '526733527',
+        steam: 'http://steamcommunity.com/id/liuame',
+        platform: 'steam',
+        wanted: 'Don\'t Starve Together, GTA5,'
     },
     {
-        name:'猫先生',
-        qq:'1551512519',
-        steam:'http://steamcommunity.com/id/shenyuahanlang',
-        platform:'steam',
-        wanted:'Don\'t Starve Together, Terraria, Tom Clancy\'s The Division'
-    },{
-        name:'呀！韬韬~',
-        qq:'521266013',
-        steam:'',
-        platform:'xbox(SUNTAO0314)',
-        wanted:'FIFA17(xbox), Dying Light(xbox), Overwatch(xbox), Rainbow Six Siege(xbox)'
-    },{
-        name:'阿豪',
-        qq:'198560609',
-        steam:'http://steamcommunity.com/profiles/76561198130311114',
-        platform:'steam',
-        wanted:'lol, Rainbow Six Siege'
+        name: '猫先生',
+        qq: '1551512519',
+        steam: 'http://steamcommunity.com/id/shenyuahanlang',
+        platform: 'steam',
+        wanted: 'Don\'t Starve Together, Terraria, Tom Clancy\'s The Division'
+    }, {
+        name: '呀！韬韬~',
+        qq: '521266013',
+        steam: '',
+        platform: 'xbox(SUNTAO0314)',
+        wanted: 'FIFA17(xbox), Dying Light(xbox), Overwatch(xbox), Rainbow Six Siege(xbox)'
+    }, {
+        name: '阿豪',
+        qq: '198560609',
+        steam: 'http://steamcommunity.com/profiles/76561198130311114',
+        platform: 'steam',
+        wanted: 'lol, Rainbow Six Siege'
+    }, {
+        name: '-',
+        qq: '1051109183',
+        steam: 'http://steamcommunity.com/profiles/76561198263992271',
+        platform: 'steam',
+        wanted: 'GTA5'
     }]
 }
