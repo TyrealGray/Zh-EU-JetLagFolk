eeabe3d3cc8778d7a22f6a62a03b56e2341e467a
tyrealgray <tyrealgray@ymail.com>
2017-01-28 12:44:18 +0100

    add friend link switch to https

--- assets/js/data/folks.js
+++ assets/js/data/folks.js
@@ -2,25 +2,25 @@ export default {
     folks: [{
         name: '司马缸',
         qq: '312846541',
-        steam: 'http://steamcommunity.com/id/tyrealgray',
+        steam: 'https://steamcommunity.com/id/tyrealgray',
         platform: 'xbox(TyrealGray), steam',
         wanted: 'Battlerite, HELLDIVERS, Rainbow Six Siege(xbox), Dying Light(xbox), Overwatch(xbox)'
     }, {
         name: '郭一年',
         qq: '971395764',
-        steam: 'http://steamcommunity.com/id/guoyinian',
+        steam: 'https://steamcommunity.com/id/guoyinian',
         platform: 'steam',
         wanted: 'Battlerite, HELLDIVERS, Terraria'
     }, {
         name: '柴鱼精',
         qq: '823404031',
-        steam: 'https://steamcommunity.com/profiles/76561198336102572',
+        steam: 'httpss://steamcommunity.com/profiles/76561198336102572',
         platform: 'steam',
         wanted: 'Dead by Daylight, Don\'t Starve Together'
     }, {
         name: 'HUIB',
         qq: '544040151',
-        steam: 'http://steamcommunity.com/profiles/76561198250406440',
+        steam: 'https://steamcommunity.com/profiles/76561198250406440',
         platform: 'steam',
         wanted: 'Battlerite'
     },
@@ -28,7 +28,7 @@ export default {
         name: 'WGL',
         qq: '380866845',
         platform: 'ps(xxzlu2) ,steam',
-        steam: 'http://steamcommunity.com/profiles/76561198079607263',
+        steam: 'https://steamcommunity.com/profiles/76561198079607263',
         wanted: 'Battlerite'
     },
     {
@@ -42,50 +42,50 @@ export default {
         name: '小迷妹',
         qq: '564759051',
         platform: 'steam',
-        steam: 'http://steamcommunity.com/profiles/76561198330683065',
+        steam: 'https://steamcommunity.com/profiles/76561198330683065',
         wanted: 'Dead by Daylight, Don\'t Starve Together, Battlerite'
     },
     {
         name: 'Gin',
         qq: '191619884',
-        steam: 'http://steamcommunity.com/profiles/76561198089957998',
+        steam: 'https://steamcommunity.com/profiles/76561198089957998',
         platform: 'steam',
         wanted: 'astroneer, GTA5, HELLDIVERS, Terraria'
     }, {
         name: '横空飞禹',
         qq: '526733527',
-        steam: 'http://steamcommunity.com/id/liuame',
+        steam: 'https://steamcommunity.com/id/liuame',
         platform: 'steam',
         wanted: 'Don\'t Starve Together, GTA5,  ARK, Dota2, Astroneer, Dying Light, Torchlight2, Dead by Daylight'
     },
     {
         name: '猫先生',
         qq: '1551512519',
-        steam: 'http://steamcommunity.com/id/shenyuahanlang',
+        steam: 'https://steamcommunity.com/id/shenyuahanlang',
         platform: 'steam',
         wanted: 'Don\'t Starve Together, Terraria, Tom Clancy\'s The Division'
     }, {
         name: '呀！韬韬~',
         qq: '521266013',
-        steam: 'http://steamcommunity.com/id/76561198303950151/',
+        steam: 'https://steamcommunity.com/id/76561198303950151/',
         platform: 'xbox(SUNTAO0314), steam',
         wanted: 'FIFA17(xbox), Dying Light(xbox), Overwatch(xbox), Rainbow Six Siege(xbox), HELLDIVERS'
     }, {
         name: '阿豪',
         qq: '198560609',
-        steam: 'http://steamcommunity.com/profiles/76561198130311114',
+        steam: 'https://steamcommunity.com/profiles/76561198130311114',
         platform: 'steam',
         wanted: 'lol, Rainbow Six Siege'
     }, {
         name: '-',
         qq: '1051109183',
-        steam: 'http://steamcommunity.com/profiles/76561198263992271',
+        steam: 'https://steamcommunity.com/profiles/76561198263992271',
         platform: 'steam',
         wanted: 'GTA5'
     }, {
         name: '怪蜀黍',
         qq: '496393688',
-        steam: 'http://steamcommunity.com/id/496393688/',
+        steam: 'https://steamcommunity.com/id/496393688/',
         platform: 'steam',
         wanted: 'GTA5'
     }, {
@@ -109,55 +109,55 @@ export default {
     }, {
         name: 'Si Seulement',
         qq: '573949138',
-        steam: 'http://steamcommunity.com/profiles/76561198254542654',
+        steam: 'https://steamcommunity.com/profiles/76561198254542654',
         platform: 'steam',
         wanted: 'Battlerite'
     }, {
         name: 'K',
         qq: '137107209',
-        steam: 'http://steamcommunity.com/profiles/76561198112197186',
+        steam: 'https://steamcommunity.com/profiles/76561198112197186',
         platform: 'steam',
         wanted: 'GTA5'
     }, {
         name: '都是虚名',
         qq: '2780353702',
-        steam: 'http://steamcommunity.com/profiles/76561198153355856/',
+        steam: 'https://steamcommunity.com/profiles/76561198153355856/',
         platform: 'steam',
         wanted: 'Battlerite, Battlefield 1'
     }, {
         name: 'Samael`',
         qq: '379326483',
-        steam: 'http://steamcommunity.com/profiles/76561198357662869/',
+        steam: 'https://steamcommunity.com/profiles/76561198357662869/',
         platform: 'steam',
         wanted: ''
     }, {
         name: 'b',
         qq: '469963000',
-        steam: 'http://steamcommunity.com/profiles/76561198269542454/',
+        steam: 'https://steamcommunity.com/profiles/76561198269542454/',
         platform: 'steam',
         wanted: 'GTA5'
     }, {
         name: 'WooooPrime',
         qq: '642404169',
-        steam: 'http://steamcommunity.com/id/WooooPrime',
+        steam: 'https://steamcommunity.com/id/WooooPrime',
         platform: 'steam',
         wanted: 'Counter-Strike: Global Offensive'
     }, {
         name: 'Aki',
         qq: '290197985',
-        steam: 'http://steamcommunity.com/id/tiaratian/',
+        steam: 'https://steamcommunity.com/id/tiaratian/',
         platform: 'steam',
         wanted: 'Don\'t Starve Together, Starbound'
     }, {
         name: ',p傻缺失,〃',
         qq: '643836106',
-        steam: 'http://steamcommunity.com/id/643836106',
+        steam: 'https://steamcommunity.com/id/643836106',
         platform: 'steam',
         wanted: 'Terraria'
     }, {
         name: '闪灵',
         qq: '305847865',
-        steam: 'http://steamcommunity.com/id/10000006',
+        steam: 'https://steamcommunity.com/id/10000006',
         platform: 'steam',
         wanted: 'GTA5'
     }]
