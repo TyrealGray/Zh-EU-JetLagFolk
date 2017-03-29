7b91d48039831be0b8c03f8bd656a6acf4f2a61d
tyrealgray <tyrealgray@ymail.com>
2017-03-29 20:50:34 +0200

    add new member

--- bundle.js
+++ bundle.js
@@ -589,6 +589,12 @@
 	        steam: 'https://steamcommunity.com/profiles/76561198148589359',
 	        platform: 'steam',
 	        wanted: 'Warframe, Trove'
+	    }, {
+	        name: '_nice to meet you',
+	        qq: '751534212',
+	        steam: 'https://steamcommunity.com/id/xiaocaomei',
+	        platform: 'steam, uplay',
+	        wanted: 'Tom Clancy\'s The Division(PC), GTA5(PC), cs:go'
 	    }]
 	};
 
