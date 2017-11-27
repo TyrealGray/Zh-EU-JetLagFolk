1310ad344aea15d4c3f6617f26abb897c2096f76
Tyreal Gray <tyrealgray@ymail.com>
2017-11-27 21:04:14 +0100

    remove firebase sdk from bundle js file

--- assets/js/data/folks.js
+++ assets/js/data/folks.js
@@ -1,21 +1,4 @@
-import * as firebase from "firebase"
+var database = firebase.database();
+var usersRef = database.ref('users');
 
-var config = {
-    apiKey: "AIzaSyBFsU5c0JjFGPv3WRRIsZNx2RpduQkzIRc",
-    authDomain: "zh-eu-folks.firebaseapp.com",
-    databaseURL: "https://zh-eu-folks.firebaseio.com",
-    projectId: "zh-eu-folks",
-    storageBucket: "zh-eu-folks.appspot.com",
-    messagingSenderId: "217229419858"
-  };
-firebase.initializeApp(config)
-firebase.auth().signInAnonymously().catch(function(error) {
-    var errorCode = error.code
-    var errorMessage = error.message
-    console.log(errorCode)
-    console.log(errorMessage)
-})
-var database = firebase.database()
-var usersRef = database.ref('users')
-
-export default usersRef
+export default usersRef;
